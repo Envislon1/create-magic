308df0b833b8a9077d38f9e08e2e9b02a842bf3d
Envislon1 <gracergysolary.ng@gmail.com>
2026-07-23 17:52:35 +0100

    dle

--- supabase/migrations/20260118205522_91e8e85f-6efa-4dd8-91d6-879795bb5bc0.sql
+++
@@ -1,4 +0,0 @@
--- Add contest start date setting
-INSERT INTO public.contest_settings (setting_key, setting_value)
-VALUES ('contest_start_date', '2026-01-08 00:00:00+01')
-ON CONFLICT (setting_key) DO NOTHING;
